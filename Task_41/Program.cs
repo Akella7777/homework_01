70a8afe767625e7f418773d67366efd03ee8c425
Mikhail Rudikov <hrom7777@mail.ru>
2023-03-02 20:33:01 +0500

    added Task_41 Task_43

---
+++ Task_41/Program.cs
@@ -0,0 +1,68 @@
+﻿// Задача 41.
+// Пользователь вводит с клавиатуры М чисел.
+// Посчитайте, сколько чисел больше 0 ввел пользователь.
+
+// Пример:
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223 -> 3
+
+Console.Clear();
+
+// ВАРИАНТ 1
+Console.Write("Введите общее количество элементов: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int quantityElements = QuantityElements(number);
+Console.WriteLine($"Количество чисел > 0 = {quantityElements}");
+
+
+int QuantityElements (int num)
+{
+    int count = 0;
+    for( int i = 0; i < num; i++)
+    {
+        Console.Write($"Введите {i + 1} число: ");
+        int element = Convert.ToInt32(Console.ReadLine());
+
+        if (element > 0) count++;
+    }
+    return count;
+}
+
+
+// ВАРИАНТ 2
+
+// Console.Write("Введите количество элементов массива: ");
+// int size = Convert.ToInt32(Console.ReadLine());
+// int[] array = CreateArray(size);
+// PrintArray(array);
+// int quantityPositivElementsArray = QuantityPositivElementsArray(array);
+// Console.WriteLine($"В массиве количество чисел > 0 = {quantityPositivElementsArray}");
+
+// int[] CreateArray(int size)
+// {
+//     int[] arr = new int[size];
+//     for (int i = 0; i < size; i++)
+//     {
+//         Console.Write($"Введите {i + 1} элемент массива: ");
+//         arr[i] = Convert.ToInt32(Console.ReadLine());
+//     }
+//     return arr;
+// }
+
+// void PrintArray(int[] arr)
+// {
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         Console.WriteLine($"index[{i}] = {arr[i]}");
+//     }
+// }
+
+// int QuantityPositivElementsArray (int[] arr)
+// {
+//     int count = 0;
+//     for( int i = 0; i < arr.Length; i++)
+//     {
+//         if (arr[i] > 0) count++;
+//     }
+//     return count;
+// }
---
+++ Task_43/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 43.
+// Напишите программу,
+// которая найдет точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+// значения k1, b1, k2, b2 задаются пользователем.
+
+// Пример:
+// k1 = 5, b1 = 2, k2 = 9, b2 = 4 -> (-0.5; -0.5)
+
+// k1 * x + b1  = k2 * x + b2;
+// k1 * x - k2 * x  = b2 - b1;
+// x = b2 - b1 / (k1 - k2);
+// x = -(b1 - b2) / (k1 - k2);
+
+double k1 = InputRead ("Введите значение k1: ");
+double b1 = InputRead ("Введите значение b1: ");
+double k2 = InputRead ("Введите значение k2: ");
+double b2 = InputRead ("Введите значение b2: ");
+
+if(k1 == k2 && b1 == b2) Console.WriteLine("Прямые проходят по идентичным точкам. Прямые совпадают.");
+else if(k1 == k2) Console.WriteLine("Прямые не имеют пересечений - прямые параллельны друг другу.");
+else
+{
+    double result1 = Math.Round(PointIntersection(k1, b1),1);
+    double result2 = Math.Round(PointIntersection(k2, b2),1);
+    Console.WriteLine($"Пересечение прямых в точке: ({result1}; {result2})");
+}
+
+double PointIntersection (double numA, double numB)
+{
+    double x = -(b1 - b2) / (k1 - k2);
+    double y = numA * x + numB;  
+    return Math.Round(y,1);
+}
+
+double InputRead(string msg)
+{
+    Console.Write(msg);
+    string value = Console.ReadLine();
+    double result = Convert.ToDouble(value);
+    return result;
+}
